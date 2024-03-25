967aecefc32af1a4796dddef853651d89b15d79b
Oladimeji Joseph <150604487+Oladimeji1609@users.noreply.github.com>
2024-03-25 13:39:08 +0100

    Update sleep_health.py

--- sleep_health.py
+++ sleep_health.py
@@ -45,7 +45,7 @@ with col2:
     Diastolic= col2.number_input('Diastolic')
 
 with col3:
-    st.image('heart-health.png', 'Welcome Dear Patient',use_container_width= False)
+    st.image('heart-health.png')
 
 st.markdown("<br>",unsafe_allow_html = True)
 st.markdown("<br>",unsafe_allow_html = True)

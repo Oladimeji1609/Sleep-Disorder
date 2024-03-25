dd1a9ebba39e74778c8261851d743089a58c08e6
Oladimeji Joseph <150604487+Oladimeji1609@users.noreply.github.com>
2024-03-25 22:00:00 +0100

    done

--- sleep_health.py
+++ sleep_health.py
@@ -17,7 +17,7 @@ with cent_co:
 
 st.markdown("<br>", unsafe_allow_html= True)
 
-st.markdown("<h1 style = 'color: #f0f2f6; text-align: center; font-family: helvetica'>Osborne Health Care</h1>", unsafe_allow_html = True)
+st.markdown("<h1 style = 'color: #05131B; text-align: center; font-family: helvetica'>Osborne Health Care</h1>", unsafe_allow_html = True)
 st.markdown("<h4 style = 'margin: -30px; color: #F11A7B; text-align: center; font-family: cursive '> Sleep Disorder Specialist </h4>", unsafe_allow_html = True)
 st.markdown("<br>", unsafe_allow_html= True)
 
